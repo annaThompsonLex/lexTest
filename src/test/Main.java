dd340cfcbb67444b98d5c12969ebc415abfd8fa6
Anna Thompson <anna.thompson.lex@gmail.com>
2017-10-31 14:24:01 +0100

    Changed class

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String message "Hejsan";
+		String message = "Hejsan";
+		String massage2 = "hej";
 
 	}
 
